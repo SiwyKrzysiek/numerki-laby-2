e09ed0af879b5bdce437f08100f66f6967f9c824
Krzysztof Dabrowski <01133318@pw.edu.pl>
2019-03-14 20:32:53 +0100

    Lepsza iteracja

--- aproksymuj.m
+++ aproksymuj.m
@@ -5,20 +5,54 @@ function wielomian = aproksymuj(x, y, stopienWyniku)
     
     X = zeros(stopienWyniku);
     
-    for r = 1:stopienWyniku
-       for k = 1:stopienWyniku
-           if r == 1 && k == 1
-               X(r, k) = n;
-               
-           else
-               potega = r-1 + k-1;
-               suma = 0;
-               for i = 1:n
-                   suma = suma + x(i)^potega;
-               end
-               
-               X(r, k) = suma;
-           end
+    %Prostrze przejscie, ale z wielokrotnym liczeniem tego samego
+%     for r = 1:stopienWyniku
+%        for k = 1:stopienWyniku
+%            if r == 1 && k == 1
+%                X(r, k) = n;
+%                
+%            else
+%                potega = r-1 + k-1;
+%                suma = 0;
+%                for i = 1:n
+%                    suma = suma + x(i)^potega;
+%                end
+%                
+%                X(r, k) = suma;
+%            end
+%        end
+%     end
+    
+    %Pierwszy rzad razem z propagacja na ukos
+    X(1, 1) = n;
+    for i = 2:stopienWyniku
+       potega = i-1;
+       suma = 0;
+       for j = 1:n
+           suma = suma + x(j)^potega;
+       end
+       
+       r = 1;
+       for k = i:-1:1
+           X(r, k) = suma;
+           
+           r = r + 1;
+       end
+    end
+    
+    %Ostatnia kolumna wraz z popagacja na ukos
+    for i = 2:stopienWyniku
+       potega = i-2 + stopienWyniku;
+       suma = 0;
+       for j = 1:n
+           suma = suma + x(j)^potega;
+       end
+       
+       k = stopienWyniku;
+       for r = i:stopienWyniku
+           X(r, k) = suma;
+           
+           k = k - 1;
        end
     end
     
